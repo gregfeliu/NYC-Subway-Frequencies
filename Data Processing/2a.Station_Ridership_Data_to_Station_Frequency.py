17a5f94de6098ab1d705cf21a98c2193b27559a7
Greg <gmf13@hampshire.edu>
2025-09-06 21:42:57 -0400

    using o-d dataset

--- Data Processing/2a.Station_Ridership_Data_to_Station_Frequency.py
+++ Data Processing/2a.Station_Ridership_Data_to_Station_Frequency.py
@@ -26,9 +26,10 @@ train_car_characteristics = train_car_characteristics.drop_duplicates(subset='ro
 ## https://data.ny.gov/Transportation/MTA-Subway-Origin-Destination-Ridership-Estimate-2/jsu2-fbtj/explore/query/SELECT%0A%20%20%60year%60%2C%0A%20%20%60day_of_week%60%2C%0A%20%20%60hour_of_day%60%2C%0A%20%20%60origin_station_complex_id%60%2C%0A%20%20sum%28%60estimated_average_ridership%60%29%20AS%20%60sum_estimated_average_ridership%60%0AGROUP%20BY%0A%20%20%60year%60%2C%0A%20%20%60day_of_week%60%2C%0A%20%20%60hour_of_day%60%2C%0A%20%20%60origin_station_complex_id%60%0AORDER%20BY%20%60sum_estimated_average_ridership%60%20DESC%20NULL%20LAST/page/aggregate
 
 try:
-    hourly_station_ridership = pd.read_csv(f"{parent_dir}/saved_data/hourly_station_ridership.csv")
+    hourly_complex_ridership = pd.read_csv(f"{parent_dir}/saved_data/hourly_complex_ridership_od.csv")
     # data comes in as an average for a week over a month so dividing by the number of months to get average per week
-    hourly_station_ridership['sum_ridership'] = hourly_station_ridership['sum_ridership'] / 12
+    hourly_complex_ridership['Estimated Average Ridership'] = hourly_complex_ridership['Estimated Average Ridership'] / 12
+    hourly_complex_ridership.columns = ['year', 'day_of_week_name', 'hour', 'complex_id', 'complex_name', 'average_ridership']
     print("Hourly ridership data loaded successfully.")
 except Exception as e:
     print(e)
@@ -66,31 +67,31 @@ complexes_w_frequency = complexes_by_dimensions.groupby(['Complex ID','day_of_we
                                                          , 'hour', 'route_id']).sum()[['frequency', 'frequency_total', 'capacity'
                                                                                         , 'frequency_capacity']].reset_index()
 
-hourly_station_ridership['departure_day'] = hourly_station_ridership.departure_day.astype(str)
-hourly_station_ridership['day_of_week'] = hourly_station_ridership['departure_day'].replace({
-                                            '0': 'Weekday', '1': 'Weekday', '2': 'Weekday'
-                                            , '3': 'Weekday', '4': 'Weekday'
-                                            , '5': 'Saturday', '6': 'Sunday'})
-hourly_station_ridership_day_type = pd.DataFrame(hourly_station_ridership.groupby(['station_complex_id'
+# hourly_complex_ridership['departure_day'] = hourly_complex_ridership.departure_day.astype(str)
+hourly_complex_ridership['day_of_week'] = hourly_complex_ridership['day_of_week_name'].replace({
+                                            'Monday': 'Weekday', 'Tuesday': 'Weekday'
+                                            , 'Wednesday': 'Weekday', 'Thursday': 'Weekday'
+                                            , 'Friday': 'Weekday'
+                                            , 'Saturday': 'Saturday', 'Sunday': 'Sunday'})
+hourly_complex_ridership_day_type = pd.DataFrame(hourly_complex_ridership.groupby(['complex_id'
                                                                       , 'day_of_week'
-                                                                      , 'departure_hour']).sum()['sum_ridership']).reset_index()
-hourly_station_ridership_day_type['sum_ridership'] = [hourly_station_ridership_day_type['sum_ridership'][idx]/5 
-                                                      if hourly_station_ridership_day_type['day_of_week'][idx]=='Weekday'
-                                                      else hourly_station_ridership_day_type['sum_ridership'][idx]
-                                                      for idx in range(len(hourly_station_ridership_day_type))]
+                                                                      , 'hour']).sum()['average_ridership']).reset_index()
+hourly_complex_ridership_day_type['average_ridership'] = [hourly_complex_ridership_day_type['average_ridership'][idx]/5 
+                                                      if hourly_complex_ridership_day_type['day_of_week'][idx]=='Weekday'
+                                                      else hourly_complex_ridership_day_type['average_ridership'][idx]
+                                                      for idx in range(len(hourly_complex_ridership_day_type))]
 
-complex_frequency_ridership = complexes_w_frequency.merge(hourly_station_ridership_day_type
+complex_frequency_ridership = complexes_w_frequency.merge(hourly_complex_ridership_day_type
                                 , left_on=['Complex ID', 'day_of_week', 'hour']
-                                , right_on=['station_complex_id', 'day_of_week', 'departure_hour'])
-complex_frequency_ridership = complex_frequency_ridership.drop(columns=['station_complex_id'
-                                                                        , 'departure_hour'])
+                                , right_on=['complex_id', 'day_of_week', 'hour'])
+complex_frequency_ridership = complex_frequency_ridership.drop(columns=['complex_id'])
 # Get frequency capacity by complex
 complex_frequency_capacity = pd.DataFrame(complex_frequency_ridership.groupby(['Complex ID', 'day_of_week', 'hour'])['frequency_capacity'].sum()).reset_index()
 complex_frequency_capacity.columns = ['Complex ID', 'day_of_week', 'hour', 'frequency_capacity_total']
 complex_frequency_ridership = complex_frequency_ridership.merge(complex_frequency_capacity, how='left', on=['Complex ID', 'day_of_week', 'hour'])
 
 # determine_train_time_intervals
-complex_frequency_ridership['route_id_ridership'] = [complex_frequency_ridership['sum_ridership'][idx] * 
+complex_frequency_ridership['route_id_ridership'] = [complex_frequency_ridership['average_ridership'][idx] * 
                                                     (complex_frequency_ridership['frequency_capacity'][idx] 
                                                      / complex_frequency_ridership['frequency_capacity_total'][idx])
                                                     for idx in range(len(complex_frequency_ridership))]
@@ -102,7 +103,7 @@ route_id_ridership['weekday_ridership_adj'] = [route_id_ridership['route_id_ride
                                                 else route_id_ridership['route_id_ridership'][idx]
                                                 for idx in range(len(route_id_ridership))]
 route_id_ridership_grouped = pd.DataFrame(route_id_ridership.groupby('route_id').sum()['weekday_ridership_adj']).reset_index()
-route_id_ridership_grouped['weekday_ridership_adj'] = (route_id_ridership_grouped['weekday_ridership_adj'] * 52.14 * 12) / 1000000
+route_id_ridership_grouped['weekday_ridership_adj'] = (route_id_ridership_grouped['weekday_ridership_adj'] * 52.14) / 1000000
 route_id_ridership_grouped.columns = ['route_id', 'yearly_ridership_MM']
 # including the average number of subway transfers for a more realistic estimate 
 # source for unlinked passenger trips: https://en.wikipedia.org/wiki/New_York_City_Subway citing https://www.apta.com/wp-content/uploads/2024-Q4-Ridership-APTA.pdf (may include SIR which is ~5.6MM)
